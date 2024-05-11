6425a175b91b7a316e5dba37deb9908f9d2e9f14
aivylv <lyvialarasati@gmail.com>
2024-05-11 11:40:25 +0700

    memeriksa apakah stack kosong atau tidak dengan fungsi isEmpty()

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -37,6 +37,16 @@ public:
 		cout << "Push value: " << value << endl;
 		return value;
 	}
+	// pop operation: remove the topmost element from the stack
+	void pop()
+	{
+		if (isEmpty())
+		{
+			cout << "Stack is empty." << endl;
+			return;
+		}
+		
+	}
 
 };
 int main()
